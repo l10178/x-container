cf7e7a74b679a5e797b060cd19095656bb5ea7e2
l10178 <l10178@163.com>
2023-11-19 17:54:11 +0800

    feat: support java debug images

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # x-container
-Build my containers.
+
+自定义容器镜像，支持常用工具类，方便快速开始和测试。
+
+## 镜像说明
+
+各镜像层级和依赖如下。
+
+```console
+[layer0]   base-debian
+           │
+[layer1]   └─── graalvm-debian
+               │
+[layer2]       └───graalvm-maven
+               │
+[layer2]       └───graalvm-tomcat
+```
+
+## 镜像构建
+
+我们使用 argo-workflows 构建镜像。你也可以参考 docker-build.sh 构建自己的镜像。
+
+## 遇到问题和解决方案
+
+- Dockerfile 内执行 curl 报错如下。
+
+    ```console
+
+    curl: (77) error setting certificate file: /etc/ssl/certs/ca-certificates.crt
+
+    ```
+
+    解决方式： 执行 `apt install ca-certificates` 命令。
---
+++ docker-build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+current_dir=$(
+    cd $(dirname $0)
+    pwd
+)
+
+cd ${current_dir}
+
+DOCKER_BASE_ORG=docker.io/nxest
+
+DOCKER_SRC=${current_dir}/src/main/docker
+
+
+APP_DOCKER_FILE=${DOCKER_SRC}/base/Dockerfile
+APP_DOCKER_TAG=${DOCKER_BASE_ORG}/base-debian:stable-slim
+APP_CONTEXT_PATH=${DOCKER_SRC}/base
+
+# export BUILDKIT_PROGRESS=plain
+
+# buildx  --no-cache  --platform linux/amd64 --push
+docker build --no-cache -t ${APP_DOCKER_TAG} -f ${APP_DOCKER_FILE} ${APP_CONTEXT_PATH}
+
+APP_DOCKER_FILE=${DOCKER_SRC}/openjdk/Dockerfile
+APP_CONTEXT_PATH=${DOCKER_SRC}/openjdk
+APP_DOCKER_TAG=${DOCKER_BASE_ORG}/openjdk:21-graal-debian
+
+docker build --no-cache -t ${APP_DOCKER_TAG} -f ${APP_DOCKER_FILE} ${APP_CONTEXT_PATH}
