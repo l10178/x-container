c2076df1707747594bef21fa7cf3ca1d93d55d75
l10178 <l10178@163.com>
2023-11-25 16:03:46 +0800

    add maven dockerfile

--- docker-build.sh
+++ docker-build.sh
@@ -7,6 +7,8 @@ current_dir=$(
 
 cd ${current_dir}
 
+export BUILDKIT_PROGRESS=plain
+
 DOCKER_BASE_ORG=docker.io/nxest
 
 DOCKER_SRC=${current_dir}/src/main/docker
@@ -16,7 +18,7 @@ APP_DOCKER_FILE=${DOCKER_SRC}/base/Dockerfile
 APP_DOCKER_TAG=${DOCKER_BASE_ORG}/base-debian:stable-slim
 APP_CONTEXT_PATH=${DOCKER_SRC}/base
 
-# export BUILDKIT_PROGRESS=plain
+
 
 # buildx  --no-cache  --platform linux/amd64 --push
 docker build --no-cache -t ${APP_DOCKER_TAG} -f ${APP_DOCKER_FILE} ${APP_CONTEXT_PATH}
@@ -24,5 +26,12 @@ docker build --no-cache -t ${APP_DOCKER_TAG} -f ${APP_DOCKER_FILE} ${APP_CONTEXT
 APP_DOCKER_FILE=${DOCKER_SRC}/openjdk/Dockerfile
 APP_CONTEXT_PATH=${DOCKER_SRC}/openjdk
 APP_DOCKER_TAG=${DOCKER_BASE_ORG}/openjdk:21-graal-debian
+
+docker build --no-cache -t ${APP_DOCKER_TAG} -f ${APP_DOCKER_FILE} ${APP_CONTEXT_PATH}
+
+
+APP_DOCKER_FILE=${DOCKER_SRC}/maven/Dockerfile
+APP_CONTEXT_PATH=${DOCKER_SRC}/maven
+APP_DOCKER_TAG=${DOCKER_BASE_ORG}/maven:3.9-openjdk21-graal-debian
 
 docker build --no-cache -t ${APP_DOCKER_TAG} -f ${APP_DOCKER_FILE} ${APP_CONTEXT_PATH}
